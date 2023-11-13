462adb5d4b43f82c78577eaea61fbe47f44c2735
David Taylor <david@taylorhq.com>
2023-11-13 15:12:31 +0000

    Bump highlightjs-glimmer to v2.2.0 (#1)
    This release splits glimmer-javascript into a separate grammar so we can drop our custom version

--- javascripts/discourse/api-initializers/init-highlightjs-glimmer.js
+++ javascripts/discourse/api-initializers/init-highlightjs-glimmer.js
@@ -1,8 +1,7 @@
 import { apiInitializer } from "discourse/lib/api";
-import glimmerJs from "../../lib/glimmer-js";
-import { glimmer } from "../../vendor/highlightjs-glimmer";
+import { glimmer, glimmerJavascript } from "../../vendor/highlightjs-glimmer";
 
 export default apiInitializer("0.8", (api) => {
   api.registerHighlightJSLanguage("glimmer", glimmer);
-  api.registerHighlightJSLanguage("gjs", glimmerJs);
+  api.registerHighlightJSLanguage("glimmer-javascript", glimmerJavascript);
 });
